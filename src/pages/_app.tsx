d629bc0a21f37c2d5b8d19871d07388613136987
Raiane <raiane.oliveira404@gmail.com>
2023-08-09 20:42:59 -0300

    feat: fonts of next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import { globalStyles } from '@/styles/global'
 import { Container } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
 
+import { Roboto } from 'next/font/google'
+
 import { CartProvider } from 'use-shopping-cart'
 import { Header } from '@/components/Header'
 
 globalStyles()
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -18,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
       currency="BRL"
       shouldPersist
     >
-      <Container>
+      <Container className={roboto.className}>
         <Header />
         <Component {...pageProps} />
       </Container>
